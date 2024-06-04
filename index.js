db5d1779b84c7c4f3e022881b6d1f0072085fc34
protim1451 <partha.doict@gmail.com>
2024-06-05 00:41:34 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('server running');
+});
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+});
